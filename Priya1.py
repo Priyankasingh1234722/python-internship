7c9c89f97f0d0c200e077c4dbd30b5dca4e6b9ee
Priyankasingh1234722 <hemanthkumarpriyanka2005@gmail.com>
2024-09-24 15:00:05 +0530

    Add files via upload

---
+++ Priya1.py
@@ -0,0 +1,19 @@
+StName=input("Enter the namre of the Student:")
+StUSN=input("Enter the USN of the Student:")
+StMarks1=int(input("Enter marks in subject:"))
+StMarks2=int(input("Enter marks in subject:"))
+StMarks3=int(input("Enter marks in subject:"))
+
+
+
+
+
+
+print("student Details\n")
+print("Name:",StName)
+print("USN:",StUSN)
+print("Marks1:",StMarks1)
+print("Marks2:",StMarks2)
+print("Marks3:",StMarks3)
+print("Total:",StMarks1+StMarks2+StMarks3)
+print("Percent:",(StMarks1+StMarks2+StMarks3)/3)
